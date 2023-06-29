5fd94204fbea94fd4c8b7bda682d6c51408124be
oksang <oksang@salin.co.kr>
2023-06-29 09:40:02 +0900

    polling 방식 제거

--- bots/telegram/index.js
+++ bots/telegram/index.js
@@ -4,10 +4,9 @@ const TelegramBot = require("node-telegram-bot-api");
 const schedule = require("node-schedule");
 const axios = require("axios");
 const { getEvents } = require("./calendarConn");
-const { startOfDay, endOfDay, format, addDays } = require("date-fns");
+const { startOfDay, endOfDay, format } = require("date-fns");
 
-// 텔레그램 봇을 생성하고, polling 방식으로 메세지를 가져옴
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN);
 
 const standEventFilter = (events) => {
   const dateVal = `<b>${format(
@@ -19,79 +18,17 @@ const standEventFilter = (events) => {
   return `${dateVal}${events.map((str) => `  ${str}\n`).join("")}`;
 };
 
-const vacationEventFilter = (events, dateTime, dayVal) => {
-  const dateVal = `<b>${format(dateTime, "yyyy년 MM월 dd일")}</b> \n\n`;
-  if (events.length === 0) return `${dateVal} ${dayVal} 휴가자는 없습니다.`;
-
-  return `${dateVal}${events.map((str) => `  ${str}\n`).join("")}`;
-};
-
-const startAndEndDayFormat = (dayVal) => {
+async function start() {
   const today = new Date();
 
-  switch (dayVal) {
-    case "어제":
-      return {
-        startDay: startOfDay(addDays(today, -1)),
-        endDay: endOfDay(addDays(today, -1)),
-      };
-    case "오늘":
-      return {
-        startDay: startOfDay(today),
-        endDay: endOfDay(today),
-      };
-    case "내일":
-      return {
-        startDay: startOfDay(addDays(today, 1)),
-        endDay: endOfDay(addDays(today, 1)),
-      };
-  }
-};
-
-async function start() {
-  // schedule.scheduleJob("1 1 10 * * *", async function () {
-  //   const event = await getEvents();
-
-  //   bot.sendMessage(process.env.BOT_ID, arrayFilter(event));
-  // });
-
-  const { startDay, endDay } = startAndEndDayFormat("오늘");
+  const startDay = startOfDay(today);
+  const endDay = endOfDay(today);
 
   const event = await getEvents(startDay, endDay);
 
   bot.sendMessage(process.env.BOT_ID, standEventFilter(event), {
     parse_mode: "HTML",
   });
-
-  // '/00휴가' 라는 명령어가 오면, 날짜에 맞는 휴가자 출력
-  bot.onText(/\/(어제|오늘|내일)휴가$/, async (msg, match) => {
-    // 'msg' : 텔레그램으로 부터 수신한 메세지
-    // 'match' : 정규식을 실행한 결과
-
-    const dayVal = match[1];
-
-    const { startDay, endDay } = startAndEndDayFormat(dayVal);
-
-    // const chatId = msg.chat.id;
-
-    const event = await getEvents(startDay, endDay);
-
-    bot.sendMessage(
-      process.env.BOT_ID,
-      vacationEventFilter(event, startDay, dayVal),
-      {
-        parse_mode: "HTML",
-      }
-    );
-  });
-
-  // .on('message')을 통해 bot이 어떤 메세지든 수신하도록 해줌
-  // bot.on("message", (msg) => {
-  //   const chatId = msg.chat.id;
-  //   console.log(chatId);
-  //   // send a message to the chat acknowledging receipt of their message
-  //   bot.sendMessage(chatId, "메세지 수신 완료!");
-  // });
 }
 
 module.exports = {
