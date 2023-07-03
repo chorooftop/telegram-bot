dd3b71f4bba3e8de3fd61389596051ecdf6ce7a8
oksang <oksang@salin.co.kr>
2023-07-03 11:17:53 +0900

    feat : console로 credentials 파일 확인

--- bots/telegram/calendarConn.js
+++ bots/telegram/calendarConn.js
@@ -52,6 +52,7 @@ async function saveCredentials(client) {
  */
 async function authorize() {
   const content = await fs.readFile(CREDENTIALS_PATH);
+  console.log("content ::", content);
   const keys = JSON.parse(content);
   console.log("keys ::", keys);
   let client = await loadSavedCredentialsIfExist();
